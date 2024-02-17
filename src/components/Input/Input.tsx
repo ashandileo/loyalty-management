239b1af9449778d65c3ae96d8add2f12b0416649
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-02-17 18:22:10 +0700

    adjust input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,7 +4,7 @@ import { IInput } from './Interface';
 const Input = ({ value, placeholder, customClass = '', onChange }: IInput) => {
   return (
     <div
-      className={`relative w-full input-component ${customClass} ${value ? 'mt-[46px]' : ''}`}
+      className={`relative w-full input-component ${customClass} ${value ? 'mt-[50px]' : ''}`}
     >
       <input
         className="border border-solid border-[#D9D9D9] outline-none p-[16px] rounded-[8px] w-full"
@@ -12,7 +12,7 @@ const Input = ({ value, placeholder, customClass = '', onChange }: IInput) => {
         onChange={onChange}
       />
       <span
-        className={`placeholder absolute left-[16px] plus-jakarta-sans text-[#9A9A9A] top-[50%] translate-y-[-50%] ${value ? 'left-0 top-[-18px]' : ''}`}
+        className={`placeholder absolute plus-jakarta-sans text-[#9A9A9A] translate-y-[-50%] ${value ? 'left-0 top-[-18px]' : 'left-[16px] top-[50%]'}`}
       >
         {placeholder}
       </span>

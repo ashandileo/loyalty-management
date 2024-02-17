5944b591065659ca9fdebaf5b3e0384c304f2f86
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-02-17 16:23:15 +0700

    configure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline mt-[100px] text-yellow-600">
-        Hello world!
-      </h1>
-    </div>
-  );
-}
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+
+const App = () => {
+  return <RouterProvider router={router} />
+};
 
 export default App;

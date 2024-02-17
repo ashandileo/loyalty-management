355989a8e81f3e974a81aa1f8467614d8384da4f
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-02-17 18:29:29 +0700

    refactor class to use ctl

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,5 @@
+import ctl from '@netlify/classnames-template-literals';
+
 import './Input.css';
 import { IInput } from './Interface';
 
@@ -8,21 +10,32 @@ const Input = ({
   onChange,
   type = 'text'
 }: IInput) => {
+  const inputContainerCN = ctl(`
+    relative
+    w-full
+    input-component
+    ${customClass}
+    ${value && 'mt-[50px]'}
+  `);
+
+  const placeholderCN = ctl(`
+    placeholder
+    absolute
+    plus-jakarta-sans
+    text-[#9A9A9A]
+    translate-y-[-50%]
+    ${value ? 'left-0 top-[-18px]' : 'left-[16px] top-[50%]'}
+  `);
+
   return (
-    <div
-      className={`relative w-full input-component ${customClass} ${value ? 'mt-[50px]' : ''}`}
-    >
+    <div className={inputContainerCN}>
       <input
         className="border border-solid border-[#D9D9D9] outline-none p-[16px] rounded-[8px] w-full"
         value={value}
         onChange={onChange}
         type={type}
       />
-      <span
-        className={`placeholder absolute plus-jakarta-sans text-[#9A9A9A] translate-y-[-50%] ${value ? 'left-0 top-[-18px]' : 'left-[16px] top-[50%]'}`}
-      >
-        {placeholder}
-      </span>
+      <span className={placeholderCN}>{placeholder}</span>
     </div>
   );
 };

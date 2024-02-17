9f5361a8f3149e5699c1dc7994db1a8dbc5f9429
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-02-17 18:06:20 +0700

    slicing signup component

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+
+import Input from '../components/Input/Input';
+import SVGIcon from '../components/SVGIcon/SVGIcon';
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="flex flex-col items-center justify-center h-[100vh]">
+      <div className="flex items-center justify-center mb-[16px]">
+        <SVGIcon
+          name="company-logo"
+          width={24}
+          height={24}
+          className="mr-[10px]"
+        />
+        <div className="text-3xl plus-jakarta-sans font-[700]">Loyauté</div>
+      </div>
+
+      <div className="w-[408px] py-[32px] px-[24px] text-center">
+        <p className="plus-jakarta-sans font-[700] text-[29px] leading-[31.9px] mb-[8px]">
+          Let’s sign You in.
+        </p>
+        <p className="plus-jakarta-sans font-[400] text-[14px] leading-[23.8px] text-[#9A9A9A] mb-[32px]">
+          Hey there, fabulous! Ready to kick back and explore? Just a quick
+          sign-in away from unlocking a world of fun.
+        </p>
+
+        <Input
+          placeholder="Email"
+          customClass="mb-[16px]"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Signup;

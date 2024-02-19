e491cb40c78483b48cd292e11b548aff4fa08965
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-02-19 11:03:57 +0700

    add header icon

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,12 @@
 import SVGIcon from '../shared/SVGIcon/SVGIcon';
 
+const listHeader = [
+  { icon: 'icon-home', name: 'Home' },
+  { icon: 'icon-calendar-2', name: 'History' },
+  { icon: 'icon-message', name: 'Inbox' },
+  { icon: 'icon-profile', name: 'Profile' }
+];
+
 const Header = () => {
   return (
     <div className="relative top-[0px] z-[10] w-full">
@@ -14,6 +21,20 @@ const Header = () => {
           <p className="text-white font-[700] text-[30.5px] leading-[33.55px]">
             Loyauté
           </p>
+        </div>
+
+        <div className="flex items-center gap-[60px]">
+          {listHeader.map((header, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-center flex-col"
+            >
+              <SVGIcon name={header.icon} width={24} height={24} />
+              <p className="plus-jakarta-sans  font-[600] text-[14px] leading-[23.1px] text-white">
+                {header.name}
+              </p>
+            </div>
+          ))}
         </div>
 
         <div className="flex items-center">
